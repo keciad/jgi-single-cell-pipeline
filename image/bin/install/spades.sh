b453f37ae3667af3a9660593fa5892335789699b
keciad <39774042+keciad@users.noreply.github.com>
2018-08-09 09:23:49 -0700

    Update spades.sh
    update spades url

--- image/bin/install/spades.sh
+++ image/bin/install/spades.sh
@@ -4,7 +4,7 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-URL="http://spades.bioinf.spbau.ru/release${SPADES_VERSION}/SPAdes-${SPADES_VERSION}-Linux.tar.gz"
+URL="http://cab.spbu.ru/files/release${SPADES_VERSION}/SPAdes-${SPADES_VERSION}-Linux.tar.gz"
 fetch_archive.sh ${URL} spades
 ln -s /usr/local/spades/bin/* /usr/local/bin
 rm -rf /usr/local/spades/share/spades/test_dataset*
